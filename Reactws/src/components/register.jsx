816930ede4020e2c170a2c39c5c01396d8b227ad
tasneembhanu <93967325+tasneembhanu@users.noreply.github.com>
2021-11-23 11:04:34 +0530

    Add files via upload

---
+++ Reactws/src/components/register.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Register extends React.Component {
+    render() { 
+        return <div>
+            <h1>Register Page</h1>
+        </div>;
+    }
+}
+ 
+export default Register;
